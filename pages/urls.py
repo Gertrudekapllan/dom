72b73ce671223ef799f1b44b6bfa8e8475e26788
Gertrudekapllan <easppolivanova@gmail.com>
2024-03-17 00:49:56 +0600

    FOOTER was added

--- pages/urls.py
+++ pages/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
-from pages import views
+from pages import views as pages_views
+from post import views as post_views
 
 urlpatterns = [
-    path('', views.home_page, name='home_page'),
-    path('services/silkscreen/', views.silkscreen, name='silkscreen'),
-    path('services/dtf-printing/', views.dtf_printing, name='dtf_printing'),
-    path('services/embroidery/', views.embroidery, name='embroidery'),
-    path('services/printing-special-effects/', views.printing_special_effects, name='printing_special_effects'),
-    path('services/individual-print/', views.individual_print, name='individual_print'),
-    path('services/sublimation_printing/', views.sublimation_printing, name='sublimation_printing'),
+    path('', pages_views.home_page, name='home_page'),
+    path('services/silkscreen/', pages_views.silkscreen, name='silkscreen'),
+    path('services/dtf-printing/', pages_views.dtf_printing, name='dtf_printing'),
+    path('services/embroidery/', pages_views.embroidery, name='embroidery'),
+    path('services/printing-special-effects/', pages_views.printing_special_effects, name='printing_special_effects'),
+    path('services/individual-print/', pages_views.individual_print, name='individual_print'),
+    path('services/sublimation_printing/', pages_views.sublimation_printing, name='sublimation_printing'),
+    path('post/<int:pk>', post_views.post_page),
 ]
