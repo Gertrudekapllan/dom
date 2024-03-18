cd692b0d40a8970d669429de9fc61401d37e418a
Gertrudekapllan <easppolivanova@gmail.com>
2024-03-18 12:13:24 +0600

    FOOTER was added

--- post/views.py
+++ post/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from post.models import Post, Image
+
+
+def post_page(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post_images = {}
+    post_images[post] = Image.objects.filter(post=post)
+    return render(request, 'post_page.html', {'post': post, 'post_images': post_images})
